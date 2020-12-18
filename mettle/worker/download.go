384d21320b26523681ca6730c09c58fa715ecca8
Peter Ebden <peter.ebden@gmail.com>
2020-12-18 12:19:00 +0000

    Compress batch downloads

--- mettle/worker/download.go
+++ mettle/worker/download.go
@@ -163,36 +163,33 @@ func (w *worker) downloadFiles(filenames []string, files map[string]*pb.FileNode
 	defer func() { <-w.limiter }()
 
 	log.Debug("Downloading batch of %d files...", len(filenames))
-	digests := make([]*pb.Digest, 0, len(filenames))
+	digests := make([]sdkdigest.Digest, 0, len(filenames))
+	compressors := make([]pb.Compressor_Value, 0, len(filenames))
 	digestToFilenames := make(map[string][]string, len(filenames))
 	for _, f := range filenames {
 		d := files[f].Digest
 		filenames, present := digestToFilenames[d.Hash]
 		if !present {
-			digests = append(digests, d)
+			digests = append(digests, sdkdigest.NewFromProtoUnvalidated(d))
+			compressors = append(compressors, compressor(f))
 		}
 		digestToFilenames[d.Hash] = append(filenames, f)
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), w.timeout)
 	defer cancel()
-	resp, err := w.client.BatchReadBlobs(ctx, &pb.BatchReadBlobsRequest{
-		InstanceName: w.client.InstanceName,
-		Digests:      digests,
-	})
+	responses, err := w.client.BatchDownloadCompressedBlobs(ctx, digests, compressors)
 	if err != nil {
 		return err
 	}
-	for _, r := range resp.Responses {
-		if r.Status.Code != int32(codes.OK) {
-			return grpcstatus.Errorf(codes.Code(r.Status.Code), "Error fetching %s: %s", r.Digest.Hash, r.Status.Message)
-		} else if filenames, present := digestToFilenames[r.Digest.Hash]; !present {
-			return grpcstatus.Errorf(codes.InvalidArgument, "Unknown digest %s in response", r.Digest.Hash)
+	for dg, data := range responses {
+		if filenames, present := digestToFilenames[dg.Hash]; !present {
+			return grpcstatus.Errorf(codes.InvalidArgument, "Unknown digest %s in response", dg.Hash)
 			// The below isn't *quite* right since it assumes file modes are consistent across all files with a matching
 			// digest, which isn't actually what the protocol says....
-		} else if err := w.writeFiles(filenames, r.Data, files[filenames[0]]); err != nil {
+		} else if err := w.writeFiles(filenames, data, files[filenames[0]]); err != nil {
 			return err
 		}
-		w.cache.Set(r.Digest.Hash, r.Data, int64(len(r.Data)))
+		w.cache.Set(dg.Hash, data, int64(len(data)))
 	}
 	return nil
 }
@@ -266,18 +263,16 @@ func fileMode(isExecutable bool) os.FileMode {
 	return 0444
 }
 
+// compressor returns the compressor to use for a given filename
+func compressor(filename string) pb.Compressor_Value {
+	if shouldCompress(filename) {
+		return pb.Compressor_ZSTD
+	}
+	return pb.Compressor_IDENTITY
+}
+
 // shouldCompress returns true if the given filename should be compressed.
 func shouldCompress(filename string) bool {
 	return !(strings.HasSuffix(filename, ".zip") || strings.HasSuffix(filename, ".pex") ||
 		strings.HasSuffix(filename, ".jar") || strings.HasSuffix(filename, ".gz"))
-}
-
-// shouldCompressAll returns true if all of the given filenames should be compressed.
-func shouldCompressAll(filenames []string) bool {
-	for _, f := range filenames {
-		if shouldCompress(f) {
-			return true
-		}
-	}
-	return false
 }
