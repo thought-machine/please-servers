e5fc2e290a3e0464dcfde3ce6572aef7470bd71b
Peter Ebden <peter.ebden@gmail.com>
2021-02-27 14:07:19 +0000

    Chuck a logger in

--- mettle/omempubsub/omem.go
+++ mettle/omempubsub/omem.go
@@ -21,10 +21,13 @@ import (
 	"sync/atomic"
 	"time"
 
+	"github.com/peterebden/go-cli-init/v3"
 	"gocloud.dev/gcerrors"
 	"gocloud.dev/pubsub"
 	"gocloud.dev/pubsub/driver"
 )
+
+var log = cli.MustGetLogger()
 
 // Scheme is the URL scheme mempubsub registers its URLOpeners under on pubsub.DefaultMux.
 const Scheme = "omem"
