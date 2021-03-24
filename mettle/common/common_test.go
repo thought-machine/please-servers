bf631b8965b1acf0006e36bbff142141ed085947
Peter Ebden <peter.ebden@gmail.com>
2021-03-24 12:14:10 +0000

    gofmt

--- mettle/common/common_test.go
+++ mettle/common/common_test.go
@@ -9,7 +9,7 @@ import (
 func TestCheckPath(t *testing.T) {
 	assert.NoError(t, CheckPath("test"))
 	assert.NoError(t, CheckPath("test/test2"))
-	assert.NoError(t, CheckPath("test/./test2"))  // Bit dodgy but not actually a problem
+	assert.NoError(t, CheckPath("test/./test2")) // Bit dodgy but not actually a problem
 	assert.Error(t, CheckPath("/test"))
 	assert.Error(t, CheckPath("/.."))
 	assert.Error(t, CheckPath(".."))
