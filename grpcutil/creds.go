f4ab68de13c790cf0fbf26cf678932680c73576c
Maxime Fischer <maxime2fischer@gmail.com>
2021-11-09 14:36:47 +0000

    Add flag to specify TLS minimum version. (#148)
    * Add flag to specify TLS minimum version.
    
    * Make 1.3 default and only allow versions from 1.2.
    
    * Only allow TLS 1.2 and 1.3 versions.
    
    * Bump version to 9.6.0.
    
    Co-authored-by: maxime <maxime@thoughtmachine.net>

--- grpcutil/creds.go
+++ grpcutil/creds.go
@@ -1,20 +1,38 @@
 package grpcutil
 
 import (
+	"crypto/tls"
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 )
 
+var tlsVersions = map[string]uint16{
+	"1.2": tls.VersionTLS12,
+	"1.3": tls.VersionTLS13,
+}
+
 // OptionalTLS loads TLS certificates from the given files and attaches them as a gRPC ServerOption.
 // If both filenames are empty then no credentials will be attached.
-func OptionalTLS(keyFile, certFile string, opts ...grpc.ServerOption) []grpc.ServerOption {
+func OptionalTLS(keyFile, certFile, tlsMinVersionFlag string, opts ...grpc.ServerOption) []grpc.ServerOption {
 	if keyFile == "" && certFile == "" {
 		log.Warning("No transport security attached, will communicate in plaintext")
 		return opts
 	}
-	creds, err := credentials.NewServerTLSFromFile(certFile, keyFile)
+	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
 	if err != nil {
 		log.Fatalf("Failed to load TLS credentials: %s", err)
 	}
+	var tlsMinVersion uint16
+	if tlsMinVersionFlag != "" {
+		var ok bool
+		if tlsMinVersion, ok = tlsVersions[tlsMinVersionFlag]; !ok {
+			log.Fatalf("TLS version %s unknown", tlsMinVersionFlag)
+		}
+	}
+	creds := credentials.NewTLS(&tls.Config{
+		Certificates: []tls.Certificate{cert},
+		MinVersion:   tlsMinVersion,
+	})
 	return append(opts, grpc.Creds(creds))
 }
