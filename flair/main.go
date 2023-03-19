ff0da3a793efc43cf7cc86b4f1dff2581b231d30
Xander Garbett <xander.garbett@outlook.com>
2023-03-19 15:59:18 +0000

    Fix load balancing logic (#227)
    * Change load balancing load to be replicator, rather than Trie. Also add tests
    
    * Increment verison
    ---------
    
    Co-authored-by: Alexander Garbett <agarbett@thoughtmachine.net>

--- flair/main.go
+++ flair/main.go
@@ -60,11 +60,11 @@ func newReplicator(geometry map[string]string, replicas int, loadBalance bool) *
 	}
 	t := trie.New(func(address string) (*grpc.ClientConn, error) {
 		return grpcutil.Dial(address, opts.ConnTLS, opts.CA, opts.GRPC.TokenFile)
-	}).WithLoadBalancing(loadBalance)
+	})
 	if err := t.AddAll(geometry); err != nil {
 		log.Fatalf("Failed to create trie for provided geometry: %s", err)
 	} else if err := t.Check(); err != nil {
 		log.Fatalf("Failed to construct trie: %s", err)
 	}
-	return trie.NewReplicator(t, replicas)
+	return trie.NewReplicator(t, replicas, loadBalance)
 }
