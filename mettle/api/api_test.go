b54b774f0c39521392c6cd42c8e68042b7ce6280
maxime <maxime@thoughtmachine.net>
2023-09-25 16:50:53 +0100

    Fix tests.

--- mettle/api/api_test.go
+++ mettle/api/api_test.go
@@ -245,7 +245,7 @@ func setupServers(t *testing.T) (pb.ExecutionClient, *executor, *grpc.Server) {
 	s, lis, err := serve(grpcutil.Opts{
 		Host: "127.0.0.1",
 		Port: 0,
-	}, "", requests, responses, responses, "", true, map[string][]string{}, casaddr, false, 1)
+	}, "", requests, responses, responses, "", true, map[string][]string{}, casaddr, false, 1, 1)
 	require.NoError(t, err)
 	go s.Serve(lis)
 	conn, err := grpc.Dial(lis.Addr().String(), grpc.WithInsecure())
